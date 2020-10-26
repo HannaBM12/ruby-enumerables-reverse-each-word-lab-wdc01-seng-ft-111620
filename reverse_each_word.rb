ffb60bd0c9d0343a070f1feac0f46d9895eb1deb
Hanna Mulugeta <hannabmulugeta@gmail.com>
2020-10-26 00:45:51 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,11 @@
+
+def reverse_each_word (string)
+  array = string.split(" ")
+  reverse_array = []
+  array.each do |element|
+    reverse_array << element.reverse
+  end
+  reverse_array.join(" ")
+end
+
+  
